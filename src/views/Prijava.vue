e31aa546d8b2e4fcd118438dca2f82c99ecb6e9b
Gordana <gvukoje@student.unipu.hr>
2021-09-20 17:20:39 +0200

    Dodana log in stranica i uključen firebase

---
+++ src/views/Prijava.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class="about">
+ <h1>Pronađi kućnu pomoćnicu</h1>
+ <div class="container">
+ <div class="row">
+ <div class="col-sm"></div>
+ <div class="col-sm">
+ <form>
+ <div class="form-group">
+ <label for="exampleInputEmail1">E-mail:</label>
+ <input type="email" class="form-control"
+id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="tvoj.mail@gmail.com" />
+ </div>
+ <div class="form-group">
+ <label for="exampleInputPassword1">Lozinka:</label>
+ <input type="password" class="form-control"
+id="exampleInputPassword1" placeholder="tvojalozinka" />
+ </div>
+ <button type="submit" class="btn btn-primary">Prijavi se</button>
+ </form>
+ </div>
+ <div class="col-sm"></div>
+ </div>
+ </div>
+ </div>
+</template>
+
+<style scoped>
+
+body {
+  background-color: rgba(128, 216, 236, 0.97);
+}
+</style>
