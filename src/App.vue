0c6d89d140202d0e650636309c9dd778af7be917
Gordana <gvukoje@student.unipu.hr>
2022-03-23 14:30:55 +0100

    rad na aplikaciji, firebase i dalje ne radi

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,6 @@
 </template>
 <script>
 
-import { firebase } from "@/firebase.js"; 
-
-firebase.auth().onAuthStateChanged((user) =>
-{
-  if (user){
-    console.log('*** User',user.email);
-    store.currentUser=user.email; 
-  }
-  else {
-    console.log('*** No User');
-    store.currentUser=null;
-
-    if (router.name !== 'Prijava'){
-      router.push({name: 'Prijava'});
-    }
-  } 
-});
-
 
 </script> 
 
